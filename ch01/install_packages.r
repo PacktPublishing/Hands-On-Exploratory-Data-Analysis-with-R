c7b82d6e30454e16088c426606e59f1114c73bc9
harish-garg <harish.garg@gmail.com>
2018-10-12 11:01:38 +0530

    code

---
+++ ch01/install_packages.r
@@ -0,0 +1,16 @@
+# Package management
+
+# Get a list of installed R packages
+installed.packages()
+
+# Update Packages
+update.packages()
+
+# Install one package
+install.packages("packagename")
+
+# Install mutiple packages
+install.packages("packagename1", "packagename2")
+
+# Remove packages
+remove.packages("packagename")
