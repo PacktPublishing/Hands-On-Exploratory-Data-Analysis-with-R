e26316b50090318c094c64e3f2e2b8adf3c8178b
radhikascs <subramanian.radhika193@gmail.com>
2019-05-27 16:16:30 +0530

    Update Chapter_2.r

--- ch02/Chapter_2.r
+++ ch02/Chapter_2.r
@@ -1,35 +1,10 @@
 library(readr)
 read_csv("mtcars.csv")
 
-cols(
-mpg = col_double(),
-cyl = col_double(),
-disp = col_double(),
-hp = col_double(),
-drat = col_double(),
-wt = col_double(),
-qsec = col_double(),
-vs = col_double(),
-am = col_double(),
-gear = col_double(),
-carb = col_double()
-)
+
 
 cars_data <- read_csv(readr_example("mtcars.csv"))
 
-cols(
-mpg = col_double(),
-cyl = col_double(),
-disp = col_double(),
-hp = col_double(),
-drat = col_double(),
-wt = col_double(),
-qsec = col_double(),
-vs = col_double(),
-am = col_double(),
-gear = col_double(),
-carb = col_double()
-)
 
 
 read_csv("data.csv", skip = 2)
